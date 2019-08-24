450f565369aa949532801ac4c26977eb320f30d4
visual_community@outlook.com <visual_community@outlook.com>
2019-08-24 13:45:08 -0400

    Solucion Completa

---
+++ mvcPhone/mvcPhone/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace mvcPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcPhone.Models.Phone> Phones { get; set; }
+    }
+}
